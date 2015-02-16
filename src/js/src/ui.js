a438499c6ea69532015a9bee558714b443fcfb3a
ryelle <kelly.dwan@gmail.com>
2015-02-15 22:03:56 -0500

    Adjust reply connectors for the “reply” button

--- src/js/src/ui.js
+++ src/js/src/ui.js
@@ -8,17 +8,26 @@
 		return;
 	}
 
-	var comments = $( document.getElementById( 'comments' ).querySelectorAll( '.parent.comment' ) );
+	var comments = $( document.getElementById( 'comments' ).querySelectorAll( '.parent.comment' ) ),
+		addReplyConnectors = function( index, element ){
+			var height,
+				replies = $(element).find( '> .children > .comment > article' );
+			if ( ! replies.length ) {
+				return;
+			}
 
-	comments.each(function( index, element ){
-		var height,
-			replies = $(element).find( '> .children > .comment > article' );
-		if ( ! replies.length ) {
-			return;
-		}
+			height = ( replies.eq(-1).offset().top - $( element ).offset().top ) + 1;
+			$( '<div>' ).addClass('reply-connector').prependTo( element ).css({ height: height + 'px' });
+		};
 
-		height = ( replies.eq(-1).offset().top - $( element ).offset().top ) + 1;
-		$( '<div>' ).addClass('reply-connector').prependTo( element ).css({ height: height + 'px' });
+	comments.each( addReplyConnectors );
+
+	$( ".comment-reply-link, #cancel-comment-reply-link" ).on( 'click', function(){
+		var comment = $( this ).closest( ".parent.comment" );
+		setTimeout( function(){
+			comment.find( ".reply-connector" ).remove();
+			addReplyConnectors( 0, comment );
+		}, 100 );
 	});
 
 	$( document.getElementById( 'comments' ) ).removeClass('no-js');
