76d46e5ea7f638ae8bad5f53a922d9e6eb0bfc64
ryelle <kelly.dwan@gmail.com>
2015-02-28 12:12:05 -0500

    Add widget area to 404 page so users can add their own content, see #10

--- src/functions.php
+++ src/functions.php
@@ -112,6 +112,16 @@ function cucina_widgets_init() {
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
 	) );
+
+	register_sidebar( array(
+		'name'          => __( '404 Page', 'cucina' ),
+		'id'            => 'sidebar-3',
+		'description'   => __( 'Use this space to give visitors suggestions when they end up on a "Not Found" page.', 'cucina' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
 }
 add_action( 'widgets_init', 'cucina_widgets_init' );
 
---
+++ src/sidebar-404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @package Cucina
+ */
+
+if ( ! is_active_sidebar( 'sidebar-3' ) ) {
+	return;
+}
+?>
+
+<div id="404-widgets" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-3' ); ?>
+</div><!-- #secondary -->
