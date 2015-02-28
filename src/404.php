4b5c735b19f2b541497d12378a693a10311356c4
ryelle <kelly.dwan@gmail.com>
2015-02-28 12:11:10 -0500

    Add our header partial to 404 pages

--- src/404.php
+++ src/404.php
@@ -7,13 +7,12 @@
 
 get_header(); ?>
 
+	<?php get_template_part( 'partial/header', 'none' ); ?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'cucina' ); ?></h1>
-				</header><!-- .page-header -->
 
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'cucina' ); ?></p>
--- src/partial/header-none.php
+++ src/partial/header-none.php
@@ -1,3 +1,7 @@
 <header class="page-header">
+<?php if ( is_search() ): ?>
 	<h1 class="page-title"><?php _e( 'Nothing Found', 'cucina' ); ?></h1>
+<?php else: ?>
+	<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'cucina' ); ?></h1>
+<?php endif; ?>
 </header><!-- .page-header -->
