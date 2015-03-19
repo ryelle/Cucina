6ecaf6edc67dc5d76d7a3c791f96add905973940
ryelle <kelly.dwan@gmail.com>
2015-03-19 18:36:42 -0400

    Add infinite scroll fix

--- src/functions.php
+++ src/functions.php
@@ -137,6 +137,11 @@ function cucina_scripts() {
 
 	wp_enqueue_script( 'cucina-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	// Only load if Jetpack is active and version is >= 3.4
+	if ( defined( 'JETPACK__VERSION' ) && version_compare( JETPACK__VERSION, '3.4', '>=' ) ) {
+		wp_enqueue_script( 'museum-jetpack-fix', get_template_directory_uri() . '/js/jetpack-infscroll-fix.js', array(), CUCINA_VERSION, true );
+	}
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
